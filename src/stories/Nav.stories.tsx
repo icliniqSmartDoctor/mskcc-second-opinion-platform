e183040056412aa7879aab5d04e1776646ae6d81
icliniq <ashik.a@icliniq.com>
2023-04-21 10:50:26 +0530

    Changed css to page scope

--- src/stories/Nav.stories.tsx
+++ src/stories/Nav.stories.tsx
@@ -6,21 +6,35 @@ import { within, fireEvent, userEvent, screen } from '@storybook/testing-library
 import Nav from '../components/common/Nav';
 import * as CONSTANTS from '../constants';
 import type { Meta, StoryFn } from "@storybook/react";
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
 
 export default {
   title: 'Home/Nav',
   component: Nav,
+  viewport: {
+    viewports: INITIAL_VIEWPORTS,
+    defaultViewport: ''
+  },
 } as Meta;
 
 const Template: StoryFn = (args) => <Nav {...args} />;
 export const Navbar = Template.bind({});
 
 
-
 Navbar.play = async ({ canvasElement }) => {
   const canvas = within(canvasElement);
 
- 
+  CONSTANTS.MSKCC_MENU.map(data => {
+    Object.values(data).map(async (res) => {
+      if (Array.isArray(res)) {
+        const arrButton: any = canvas.getByText(Object.keys(data)[0] + " +").parentElement;
+
+      }
+    })
+  })
+
 
 };
+
+
 
