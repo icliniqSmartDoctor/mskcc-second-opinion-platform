395b22912ff6d3ac6d43ce0b209e46083c77d517
belsonicliniq <belson@icliniq.com>
2023-04-27 15:27:06 +0530

    new image changes

--- src/utils/AssetAccessor.tsx
+++ src/utils/AssetAccessor.tsx
@@ -1,17 +1,17 @@
-import CTA from "assets/CTA.webp"
-import mskcc_Logo from 'assets/msk_h_rgb 1.webp'
-import ic_white_logo from 'assets/ic-white-logo.webp'
-import msk_logo_white from 'assets/msk-logo-white.webp'
-import ic_blue_logo from 'assets/ic-blue-logo.webp';
+// import CTA from "assets/CTA.webp"
+// import mskcc_Logo from 'assets/msk_h_rgb 1.webp'
+// import ic_white_logo from 'assets/ic-white-logo.webp'
+// import msk_logo_white from 'assets/msk-logo-white.webp'
+// import ic_blue_logo from 'assets/ic-blue-logo.webp';
  
-// If new image added import and add it to AssetAccessor
-const AssetAccessor = {
-  CTA,
-  mskcc_Logo,
-  ic_white_logo,
-  msk_logo_white,
-  ic_blue_logo
-}
+// // If new image added import and add it to AssetAccessor
+// const AssetAccessor = {
+//   CTA,
+//   mskcc_Logo,
+//   ic_white_logo,
+//   msk_logo_white,
+//   ic_blue_logo
+// }
 
 
-export default AssetAccessor;
+// export default AssetAccessor;
