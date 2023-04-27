eb879c3936d7322d2b15b60a1a6206161619162b
belsonicliniq <belson@icliniq.com>
2023-04-27 19:32:49 +0530

    fontawesome and close function

---
+++ src/components/Home/VIdeoFloat.tsx
@@ -0,0 +1,53 @@
+import React, { useEffect, useRef } from 'react';
+
+const VideoFloat = () => {
+  const videoWrapRef = useRef<HTMLDivElement>(null);
+  const videoRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const videoWrap:HTMLDivElement | null = videoWrapRef.current;
+    const video:HTMLDivElement | null = videoRef.current;
+
+    const handleScroll = () => {
+      const windowScrollTop = window.pageYOffset;
+      if(videoWrap){
+        const videoBottom = videoWrap!.offsetTop + videoWrap.offsetHeight;
+
+        if (windowScrollTop > videoBottom) {
+          videoWrap.style.height = '0';
+          video?.classList.add('stuck');
+        } else {
+          videoWrap.style.height = 'auto';
+          video?.classList.remove('stuck');
+        }
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+  const handleVideoClose = () => {
+    const videoWrap = videoWrapRef.current;
+
+    if (videoWrap) {
+      videoWrap.style.display = 'none';
+    }
+  };
+  return (
+    <div className="video-wrap" ref={videoWrapRef}>
+		<div className="video-floating" ref={videoRef}>
+			<div className='relative w-full'>
+				<i className="icon-close-video far fa-window-close" onClick={handleVideoClose}></i>
+				<video className="w-full cursor-pointer" id='video-player' poster="https://assets.icliniq.com/mskcc-india/mskcc-india-front-pause.png"  preload='metadata' controls>
+					<source src="https://s3.amazonaws.com/images.icliniq.com/video/MSK+India+welcome+video_icliniq+site+and+phone.mp4" type="video/mp4"/>
+				</video>
+			</div>
+		</div>
+	</div>
+  );
+};
+
+export default VideoFloat;
--- src/utils/AssetAccessor.tsx
+++ src/utils/AssetAccessor.tsx
@@ -1,10 +1,12 @@
 import whatsappIcon from 'assets/whatsappIcon.svg'
 import telephoneIcon from 'assets/telephoneIcon.svg'
+import fontAwesome from 'assets/fontawesome-pro-5/css/all.min.css'
 
 // // If new image added import and add it to AssetAccessor
 const AssetAccessor = {
   whatsappIcon,
-  telephoneIcon
+  telephoneIcon,
+  fontAwesome
 }
 
 
