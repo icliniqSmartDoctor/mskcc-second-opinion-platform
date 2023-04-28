b5ddf96b8a86b33cacd75cd53c147bf216e77eab
icliniq <ashik.a@icliniq.com>
2023-04-28 16:50:38 +0530

    Changed Image

--- src/utils/AssetAccessor.tsx
+++ src/utils/AssetAccessor.tsx
@@ -1,10 +1,12 @@
 import whatsappIcon from 'assets/img/whatsappIcon.svg'
 import telephoneIcon from 'assets/img/telephoneIcon.svg'
+import mskccBuilding from "assets/img/mskcc-building-v1.webp"
 
 // // If new image added import and add it to AssetAccessor
 const AssetAccessor = {
   whatsappIcon,
-  telephoneIcon
+  telephoneIcon,
+  mskccBuilding
 }
 
 
