0cd59341e47639b79b5d1301d6f815ff991e4cd3
belsonicliniq <belson@icliniq.com>
2023-04-28 17:26:08 +0530

    footer speed

--- src/components/Common/Footer.tsx
+++ src/components/Common/Footer.tsx
@@ -18,12 +18,12 @@ function Footer() {
                             <div className='mt-10 text-gray-400 leading-24'>Our experts treat every form of cancer, including the most important one—yours. MSK is able to provide guidance and care for patients in India through our collaboration with iCliniq, a leading global telemedicine provider. Our India-based team of dedicated clinical specialists and care coordinators will help you, your family, or representative access MSK’s doctors in New York.</div>
 
                             <div className='text-md flex items-center tracking-wide	 mt-8 font-normal	'>
-                                <span className="material-symbols-outlined text-white pr-2 -mt-1 text-3xl">mail</span>
+                                <span className="material-icons-outlined text-white pr-2 -mt-1 text-3xl">mail</span>
                                 <p className='text-white'>Email: </p>
                                 <a className="pl-2 text-secondary underline" href="mailto:mskccindia@icliniq.com">mskccindia@icliniq.com</a>
                             </div>
                             <div className='text-md flex items-center tracking-wide	 mt-3 font-normal	'>
-                                <span className="material-symbols-outlined text-white pr-2 -mt-1 text-3xl">call</span>
+                                <span className="material-icons-outlined text-white pr-2 -mt-1 text-3xl">call</span>
                                 <p className='text-white'>Phone Number: </p>
                                 <a className="pl-2 text-secondary underline" href="tel:+918270070000">+91 82 700 70000</a>
                             </div>
@@ -38,7 +38,7 @@ function Footer() {
                                     CANCER_SPECIALITIES.map((res) => {
                                         return <div className='col-span-2 h-4/5  lg:flex flex-wrap flex-col md:col-span-1'>
                                             {Object.keys(res).map((data) =>
-                                                <div className='mt-5 w-auto break-words '><a className='text-gray-400 capitalize hover:text-gray-200' href={res[data]}>{data}</a></div>
+                                                <div className='mt-5 mr-10 w-auto break-words '><a className='text-gray-400 capitalize hover:text-gray-200' href={res[data]}>{data}</a></div>
                                             )}
                                         </div>
                                     })
