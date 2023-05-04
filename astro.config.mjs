63ad69e872410e64f754c66ada5aa1a24b356796
icliniq <ashik.a@icliniq.com>
2023-05-04 14:06:05 +0530

    Corrected Imports of Asset Accessor

--- astro.config.mjs
+++ astro.config.mjs
@@ -16,6 +16,6 @@ export default defineConfig({
     config: {
       forward: ["dataLayer.push"]
     }
-  }), 
+  }) 
 ]
 });
--- src/utils/AssetAccessor.tsx
+++ src/utils/AssetAccessor.tsx
@@ -1,6 +1,7 @@
-import whatsappIcon from 'https://assets.icliniq.com/v2/assets/msk-astro/whatsappIcon.svg'
-import telephoneIcon from 'https://assets.icliniq.com/v2/assets/msk-astro/telephoneIcon.svg'
-import mskccBuilding from "https://assets.icliniq.com/v2/assets/images/mskcc/img/mskcc-cancer/img-compress/mskcc-building-v1.webp"
+
+const whatsappIcon = 'https://assets.icliniq.com/v2/assets/msk-astro/whatsappIcon.svg';
+const telephoneIcon = 'https://assets.icliniq.com/v2/assets/msk-astro/telephoneIcon.svg';
+const mskccBuilding = "https://assets.icliniq.com/v2/assets/images/mskcc/img/mskcc-cancer/img-compress/mskcc-building-v1.webp";
 
 // // If new image added import and add it to AssetAccessor
 const AssetAccessor = {
