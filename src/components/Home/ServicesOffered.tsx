0a90f95d3163e78adc5c771fad2e69e28990f0f7
belsonicliniq <belson@icliniq.com>
2023-05-04 12:49:17 +0530

    discernible name  added

--- src/components/Home/ServicesOffered.tsx
+++ src/components/Home/ServicesOffered.tsx
@@ -3,11 +3,11 @@ function ServicesOffered() {
     <div className="grid grid-cols-1 md:grid-cols-2 gap-8 items-center">
         <div className="w-full bg-white  rounded-lg dark:bg-gray-800 dark:border-gray-700 h-full relative">
             <div className='p-10 pb-20'>
-                <a href='mskcc-india-services/remote-second-opinion'>
-                    <img className="rounded-t-lg w-full" src='https://assets.icliniq.com/v2/assets/images/mskcc/img/mskcc-cancer/mskcc-remote.png' alt="" />
+                <a href='mskcc-india-services/remote-second-opinion' aria-label="remote-second-opinion">
+                    <img className="rounded-t-lg w-full" src='https://assets.icliniq.com/v2/assets/images/mskcc/img/mskcc-cancer/mskcc-remote.png' alt="remote-second-opinion" title="remote-second-opinion" />
                 </a>
                 <div className="pt-5 text-center mt-2">
-                    <a href='mskcc-india-services/remote-second-opinion'>
+                    <a href='mskcc-india-services/remote-second-opinion' aria-label="remote-second-opinion">
                         <h5 className="mb-2 text-2xl font-semibold hover:text-secondary transition-colors duration-500   text-gray-700 dark:text-white">Remote Second Opinions from MSK Oncologists</h5>
                     </a>
                     <p className="my-3 text-md">Comprehensive and customized remote second opinion package, depending on your cancer diagnosis and care needs</p>
@@ -19,8 +19,8 @@ function ServicesOffered() {
         </div>
         <div className="w-full bg-white  rounded-lg dark:bg-gray-800 dark:border-gray-700 h-full relative">
             <div className='p-10 pb-20'>
-                <a href='mskcc-india-services/travel-guide-to-mskcc-new-york'>
-                    <img className="rounded-t-lg w-full" src='https://assets.icliniq.com/v2/assets/images/mskcc/img/mskcc-cancer/mskcc-travel.png' alt="" />
+                <a href='mskcc-india-services/travel-guide-to-mskcc-new-york' aria-label="mskcc-travel">
+                    <img className="rounded-t-lg w-full" src='https://assets.icliniq.com/v2/assets/images/mskcc/img/mskcc-cancer/mskcc-travel.png' alt="mskcc-travel" title="mskcc-travel" />
                 </a>
                 <div className="pt-5 text-center mt-2">
                     <a href='mskcc-india-services/travel-guide-to-mskcc-new-york'>
