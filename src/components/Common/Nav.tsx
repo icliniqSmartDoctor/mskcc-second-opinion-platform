6ce3b96963dd8aa2e0a9342bab1202a94b9cf2e0
belsonicliniq <belson@icliniq.com>
2023-05-04 13:19:42 +0530

    font optimise

--- src/components/Common/Nav.tsx
+++ src/components/Common/Nav.tsx
@@ -38,12 +38,12 @@ function Nav() {
 
                     <Navbar.Collapse className='hidden md:hidden lg:block'>
                         <div className='flex items-center tracking-wide font-normal	'>
-                            <span className="material-icons-outlined text-primary pr-2 -mt-1 text-3xl">mail</span>
+                            <svg xmlns="http://www.w3.org/2000/svg" className='mr-2' fill="#002569" height="30" viewBox="0 96 960 960" width="30"><path d="M140 896q-24 0-42-18t-18-42V316q0-24 18-42t42-18h680q24 0 42 18t18 42v520q0 24-18 42t-42 18H140Zm340-302L140 371v465h680V371L480 594Zm0-60 336-218H145l335 218ZM140 371v-55 520-465Z"/></svg>
                             <p className='text-primary text-base'>Email: </p>
                             <a className="pl-2 text-secondary text-base underline hover:no-underline" href="mailto:mskccindia@icliniq.com">mskccindia@icliniq.com</a>
                         </div>
                         <div className='flex items-center tracking-wide font-normal'>
-                            <span className="material-icons-outlined text-primary pr-2 -mt-1 text-3xl">call</span>
+                            <svg xmlns="http://www.w3.org/2000/svg" className='mr-2' fill="#002569" height="30" viewBox="0 96 960 960" width="30"><path d="M795 936q-122 0-242.5-60T336 720q-96-96-156-216.5T120 261q0-19.286 12.857-32.143T165 216h140q13.611 0 24.306 9.5Q340 235 343 251l27 126q2 14-.5 25.5T359 422L259 523q56 93 125.5 162T542 802l95-98q10-11 23-15.5t26-1.5l119 26q15.312 3.375 25.156 15.188Q840 740 840 756v135q0 19.286-12.857 32.143T795 936ZM229 468l81-82-23-110H180q0 39 12 85.5T229 468Zm369 363q41 19 89 31t93 14V769l-103-21-79 83ZM229 468Zm369 363Z"/></svg>
                             <p className='text-primary text-base'>Phone Number: </p>
                             <a className="pl-2 text-secondary text-base underline hover:no-underline" href="tel:+918270070000">+91 82 700 70000</a>
                         </div>
