c7074a709765693a1e43a11aeb4e818530a71925
belsonicliniq <belson@icliniq.com>
2023-05-04 13:45:11 +0530

    AssetAccessor

--- src/utils/AssetAccessor.tsx
+++ src/utils/AssetAccessor.tsx
@@ -1,6 +1,6 @@
-import whatsappIcon from 'assets/img/whatsappIcon.svg'
-import telephoneIcon from 'assets/img/telephoneIcon.svg'
-import mskccBuilding from "assets/img/mskcc-building-v1.webp"
+import whatsappIcon from 'https://assets.icliniq.com/v2/assets/msk-astro/whatsappIcon.svg'
+import telephoneIcon from 'https://assets.icliniq.com/v2/assets/msk-astro/telephoneIcon.svg'
+import mskccBuilding from "https://assets.icliniq.com/v2/assets/images/mskcc/img/mskcc-cancer/img-compress/mskcc-building-v1.webp"
 
 // // If new image added import and add it to AssetAccessor
 const AssetAccessor = {
