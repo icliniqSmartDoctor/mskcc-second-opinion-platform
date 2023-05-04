9cc55a54b428f6cece8cb8f61102a7cd9822c362
icliniq <ashik.a@icliniq.com>
2023-05-04 12:36:06 +0530

    Minor Updates

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,14 +3,16 @@ module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}', 'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
 	],
 	theme: {
-		extend: {},
+		extend: {
+		},
 		colors: {
 			primary: '#002569',
 			secondary: '#0073E0',
 			darkblue: '#223645',
 		},
 		fontFamily: {
-			'rubik': ['rubik','sans-serif']
+			'rubik': ['rubik', 'sans-serif'],
+			'poppins': ['poppins']
 		}
 	},
 	plugins: [require('flowbite/plugin')],
