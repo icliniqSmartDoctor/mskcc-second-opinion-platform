9f1f1d56b6c1b18a00d0e984347283e51cb044a2
thilak <thilakcric946@gmail.com>
2023-05-05 18:23:37 +0530

    update test script | Thilak

--- tests/scripts/test.sh
+++ tests/scripts/test.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
-
-pnpm install http-server
-pnpm install wait-on
-pnpm install concurrently
-
 pnpm run build-storybook --quiet
 
 trap 'kill $(jobs -p)' EXIT
 
-npx concurrently -k -s first -n "SB,TEST" -c "magenta,blue" \
-  "npx http-server storybook-static --port 6006 --silent --no-clipboard" \
-  "npx wait-on tcp:6006 && pnpm run test-storybook"
+npx http-server storybook-static --port 6006 --silent --no-clipboard &
+
+while ! nc -z localhost 6006; do
+  sleep 1
+done
+
+pnpm run test-storybook
 
