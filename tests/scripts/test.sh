5b9f3acff5d1025aa1bf8d13876355a7de868677
thilak <thilakcric946@gmail.com>
2023-05-05 17:58:26 +0530

    install concurrently before running test | Thilak

--- tests/scripts/test.sh
+++ tests/scripts/test.sh
@@ -2,16 +2,10 @@
 
 pnpm install http-server
 pnpm install wait-on
+pnpm install concurrently
 
-
-
-# Install Playwright
-# npx playwright install --with-deps
-
-# Build Storybook
 pnpm run build-storybook --quiet
 
-# Serve Storybook and run tests
 trap 'kill $(jobs -p)' EXIT
 
 npx concurrently -k -s first -n "SB,TEST" -c "magenta,blue" \
