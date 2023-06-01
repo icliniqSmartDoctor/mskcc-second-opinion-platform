6b1e4c4ccc3240d4e8e6bac85694ec24fa885c60
icliniq <ashik.a@icliniq.com>
2023-06-01 10:24:02 +0530

    Reverted unwanted changes

--- src/components/Common/Nav.tsx
+++ src/components/Common/Nav.tsx
@@ -8,18 +8,7 @@ import { useEffect, useState } from 'react';
 
 function Nav() {
 
-    const [resp, setResp] = useState(null)
-    useEffect(() => {
-
-        async function fetchApi() {
-
-            const factResponse = await fetch('https://catfact.ninja/fact');
-            const factData = await factResponse.json();
-            setResp(factData)
-        }
-        fetchApi()
-
-    }, [])
+ 
 
     const openAccordation = () => {
         const customAccordation = document.getElementsByClassName('custom-accordation')[0];
@@ -51,7 +40,6 @@ function Nav() {
                         </div>
                         <Navbar.Toggle className='block md:!block lg:!hidden' onClick={openAccordation} />
                     </div>
-{resp && resp.fact}
                     <Navbar.Collapse className='hidden md:hidden lg:block'>
                         <div className='flex items-center tracking-wide font-normal	'>
                             <svg xmlns="http://www.w3.org/2000/svg" className='mr-2' fill="#002569" height="30" viewBox="0 96 960 960" width="30"><path d="M140 896q-24 0-42-18t-18-42V316q0-24 18-42t42-18h680q24 0 42 18t18 42v520q0 24-18 42t-42 18H140Zm340-302L140 371v465h680V371L480 594Zm0-60 336-218H145l335 218ZM140 371v-55 520-465Z" /></svg>
